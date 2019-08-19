aef3e4e440025db5ec89b0b28921a12852e16edd
kaldalis <yudtpb@gmail.com>
2019-08-19 12:58:26 -0400

    adicionado troca de card na mesma list

--- src/Components/Card/index.js
+++ src/Components/Card/index.js
@@ -1,13 +1,13 @@
-import React, {useRef} from 'react';
+import React, {useRef, useContext} from 'react';
 import { useDrag, useDrop } from "react-dnd";
 import { Container, Label } from './styles';
-
-export default function Card({data, index}) {
+import BoardContext from "../Board/context";
+export default function Card({data, index, listIndex}) {
 
   const ref = useRef();
-
+  const {move} = useContext(BoardContext);
   const [{isDragging}, dragRef] = useDrag({
-    item: {type: 'CARD', index},
+    item: {type: 'CARD', index, listIndex},
     collect: monitor => ({
       isDragging: monitor.isDragging(),
     }),
@@ -16,7 +16,28 @@ export default function Card({data, index}) {
   const [, dropRef ] = useDrop({
     accept: 'CARD',
     hover(item, monitor) {
-      console.log(item.index + " => " + index);
+      const draggedListIndex = item.listIndex;
+      // const targetListIndex = listIndex;
+      const draggedIndex = item.index;
+      const targetIndex = index;
+
+      if (draggedIndex === targetIndex) {
+        return;
+      }
+      const targetSize = ref.current.getBoundingClientRect();
+      const targetCenter = (targetSize.bottom - targetSize.top)/2;
+      const draggedOffset = monitor.getClientOffset();
+      const draggedTop = draggedOffset.y - targetSize.top;
+
+      if (draggedIndex < targetIndex && draggedTop < targetCenter) {
+        return;
+      }
+
+      if (draggedIndex > targetIndex && draggedTop > targetCenter) {
+        return;
+      }
+      move(draggedListIndex ,draggedIndex, targetIndex);
+      item.index = targetIndex;
     }
   });
 
